de6445c0a37dcf498e20782630f21e6164c5d78d
iboyeon0816 <iboyeon0816@gmail.com>
2025-02-26 17:15:02 +0900

    test(6): CommentService의 댓글 조회 기능 테스트 추가

--- src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
+++ src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
@@ -1,23 +1,27 @@
 package org.example.expert.domain.comment.service;
 
 import org.example.expert.domain.comment.dto.request.CommentSaveRequest;
+import org.example.expert.domain.comment.dto.response.CommentResponse;
 import org.example.expert.domain.comment.dto.response.CommentSaveResponse;
 import org.example.expert.domain.comment.entity.Comment;
 import org.example.expert.domain.comment.repository.CommentRepository;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.common.exception.InvalidRequestException;
-import org.example.expert.domain.common.exception.ServerException;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.repository.TodoRepository;
 import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.enums.UserRole;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.LongStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -70,5 +74,43 @@ class CommentServiceTest {
 
         // then
         assertNotNull(result);
+    }
+
+    @Test
+    @DisplayName("댓글 조회 성공")
+    void getCommentsSuccess() {
+        // given
+        long todoId = 1;
+        Todo todo = new Todo();
+        List<Comment> commentList = LongStream.rangeClosed(1, 3)
+                .mapToObj(id -> createCommentWithNewUser(id, todo))
+                .toList();
+
+        given(commentRepository.findByTodoIdWithUser(todoId)).willReturn(commentList);
+
+        // when
+        List<CommentResponse> resultList = commentService.getComments(todoId);
+
+        //then
+        assertNotNull(resultList);
+        assertEquals(commentList.size(), resultList.size());
+
+        for (int i = 0; i < commentList.size(); i++) {
+            assertCommentEquals(commentList.get(i), resultList.get(i));
+        }
+    }
+
+    private Comment createCommentWithNewUser(long id, Todo todo) {
+        User user = User.fromAuthUser(new AuthUser(id, "temp" + id + "@example.com", UserRole.USER));
+        Comment comment = new Comment("contents" + id, user, todo);
+        ReflectionTestUtils.setField(comment, "id", id);
+        return comment;
+    }
+
+    private void assertCommentEquals(Comment expected, CommentResponse result) {
+        assertEquals(expected.getId(), result.getId());
+        assertEquals(expected.getContents(), result.getContents());
+        assertEquals(expected.getUser().getId(), result.getUser().getId());
+        assertEquals(expected.getUser().getEmail(), result.getUser().getEmail());
     }
 }
